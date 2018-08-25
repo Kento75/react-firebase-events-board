93a3cde30b120951a52bdcb93c50803e43c764ae
Kento75 <kento2github@gmail.com>
2018-08-25 19:27:49 +0900

    Fix adding onclick events

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -56,15 +56,32 @@ const eventsDashboard = [
 
 
 class EventDashboard extends Component {
+  state = {
+    events: eventsDashboard,
+    isOpen: false,
+  }
+
+  handleFormOpen = () => {
+    this.setState({
+      isOpen: true,
+    })
+  }
+
+  handleCancel = () => {
+    this.setState({
+      isOpen: false,
+    })
+  }
+
   render() {
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={eventsDashboard} />
+          <EventList events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button positive content="Create Event" />
-          <EventForm />
+          <Button onClick={this.handleFormOpen} positive content="Create Event" />
+          {this.state.isOpen && <EventForm handleCancel={this.handleCancel} />}
         </Grid.Column>
       </Grid>
 
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -3,6 +3,9 @@ import { Segment, Form, Button } from "semantic-ui-react";
 
 class EventForm extends Component {
   render() {
+
+    const {handleCancel} = this.props
+
     return (
       <Segment>
         <Form>
@@ -29,7 +32,7 @@ class EventForm extends Component {
           <Button positive type="submit">
             Submit
           </Button>
-          <Button type="button">Cancel</Button>
+          <Button onClick={handleCancel} type="button">Cancel</Button>
         </Form>
       </Segment>
     )
