6fd9085cb82b870885d33a8eaa1401925276e54f
Kento75 <kento2github@gmail.com>
2018-08-25 14:34:59 +0900

    Fix home mock

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(<App />, rootEl)
@@ -16,6 +17,6 @@ if (module.hot) {
   })
 }
 
-render();
+render()
 
 registerServiceWorker();
