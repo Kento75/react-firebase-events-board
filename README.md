a725424bbc95b6829e3888d5a854df26ea53ce9a
Kento75 <kento2github@gmail.com>
2018-08-26 09:02:57 +0900

    Fix create and update logic

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # react-firebase-example
 
+## node and npm version
+* node version 8.11.4 LTS  
+* npm version 5.6.0  
+
+## Global install packages
+* create-react-app  
+
+## VScode packages
+* Auto Rename Tag  
+* Bracket Pair Colorizer  
+* Debugger for Chrome  
+* ES7 React/Redux/GraphQL/React-Native snippets  
+* ESLint  
+* JavaScript(ES6) code snippets  
+* Material Icon Theme  
+* npm Intellisense  
+* Path Intellisense  
+* Prettier-Code formatter  
+* React Redux Firestore Course Snippets  
+
 ## stack
 * React16  
 * Redux16  
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -60,10 +60,12 @@ class EventDashboard extends Component {
   state = {
     events: eventsDashboard,
     isOpen: false,
+    selectedEvent: null,
   }
 
   handleFormOpen = () => {
     this.setState({
+      selectedEvent: null,
       isOpen: true,
     })
   }
@@ -71,6 +73,27 @@ class EventDashboard extends Component {
   handleCancel = () => {
     this.setState({
       isOpen: false,
+    })
+  }
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if(event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent)
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null,
+    })
+  }
+
+  handleOpenEvent = (eventToOpen) => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true,
     })
   }
 
@@ -85,14 +108,15 @@ class EventDashboard extends Component {
   }
 
   render() {
+    const {selectedEvent} = this.state
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={this.state.events} />
+          <EventList onEventOpen={this.handleOpenEvent} events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
-          {this.state.isOpen && <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
+          {this.state.isOpen && <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
         </Grid.Column>
       </Grid>
 
