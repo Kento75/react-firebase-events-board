08134feb27257a5a0542a7bd99dc2e83ce61ef21
Kento75 <kento2github@gmail.com>
2018-08-26 15:06:21 +0900

    Fix delete events

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,12 +3,17 @@ import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const {events, onEventOpen} = this.props;
+    const {events, onEventOpen, deleteEvent} = this.props;
     return (
       <div>
         <h1>Event List</h1>
         {events.map((event) => (
-          <EventListItem key={event.id}  event={event} onEventOpen={onEventOpen} />
+          <EventListItem
+            key={event.id}
+            event={event}
+            onEventOpen={onEventOpen}
+            deleteEvent={deleteEvent}
+          />
         ))}
       </div>
     )
