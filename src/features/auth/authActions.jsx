c2441f7d1cd52245650935fe61e5ee5fd08db430
Kento75 <kento2github@gmail.com>
2018-09-04 22:00:27 +0900

    Add Auth login logic

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,15 +1,49 @@
-import { LOGIN_USER, SIGN_OUT_USER } from "./authConstants";
+import { SubmissionError } from "redux-form";
 import { closeModal } from "../modals/modalActions";
 
-export const login = (creds) => {
-  return dispatch => {
-    dispatch({type: LOGIN_USER, payload: {creds}})
-    dispatch(closeModal())
-  }
-}
+export const login = creds => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(creds.email, creds.password);
+      dispatch(closeModal());
+    } catch (error) {
+      console.log(error);
+      throw new SubmissionError({
+        _error: "Login failed"
+      });
+    }
 
-export const logout = () => {
-  return {
-    type: SIGN_OUT_USER
+    dispatch(closeModal());
+  };
+};
+
+export const registerUser = (user) => 
+  async(dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      // create the user in auth
+      let createdUser = await firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+      console.log(createdUser);
+      // update the auth profile
+      await createdUser.updateProfile({
+        displayName: user.displayName
+      })
+      // create a new profile in firestore
+      let newUser = {
+        displayName: user.displayName,
+        createdAt: firestore.FieldValue.serverTimestamp()
+      }
+      await firestore.set(`users/${createdUser.id}`, {...newUser});
+      dispatch(closeModal());
+    } catch(error) {
+      console.log(error);
+      throw new SubmissionError({
+        _error: error.message
+      });
+    }
   }
-}
+
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { withFirebase } from "react-redux-firebase";
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from "../../modals/modalActions";
-import { logout } from "../../auth/authActions";
 
 const actions = {
   openModal,
-  logout,
 }
 
 const mapState = (state) => ({
-  auth: state.auth
+  auth: state.firebase.auth
 })
 
 class NavBar extends Component {
@@ -27,13 +26,13 @@ class NavBar extends Component {
   }
 
   handleSignOut = () => {
-    this.props.logout();
+    this.props.firebase.logout();
     this.props.history.push('/');
   };
 
   render() {
     const { auth } = this.props;
-    const authenticated = auth.authenticated;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -58,7 +57,7 @@ class NavBar extends Component {
             />
           </Menu.Item>}
           {authenticated ? (
-            <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut} />
+            <SignedInMenu auth={auth} signOut={this.handleSignOut} />
           ) : (
             <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />
           )}
@@ -68,4 +67,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(withFirebase(connect(mapState, actions)(NavBar)));
