107932d1ba562ea810bcfafd44b035cdff849075
Kento75 <kento2github@gmail.com>
2018-09-08 18:24:58 +0900

    Add Firestore image file upload logic

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   Image,
   Segment,
@@ -6,10 +7,68 @@ import {
   Divider,
   Grid,
   Button,
+  Icon,
   Card
 } from "semantic-ui-react";
+import Dropzone from "react-dropzone";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { toastr } from "react-redux-toastr";
+import { uploadProfileImage } from "../userActions";
+
+const actions = {
+  uploadProfileImage
+};
 
 class PhotosPage extends Component {
+  state = {
+    files: [],
+    fileName: "",
+    cropResult: null,
+    image: {}
+  };
+
+  uploadImage = async () => {
+    try {
+      await this.props.uploadProfileImage(
+        this.state.image,
+        this.state.fileName
+      );
+      this.cancelCrop();
+      toastr.success("Success!", "Photo has been uploaded");
+    } catch (error) {
+      toastr.error("Oops", error.message);
+    }
+  };
+
+  cancelCrop = () => {
+    this.setState({
+      files: [],
+      image: {}
+    });
+  };
+
+  cropImage = () => {
+    if (typeof this.refs.cropper.getCroppedCanvas() === "undefined") {
+      return;
+    }
+
+    this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+      let imageUrl = URL.createObjectURL(blob);
+      this.setState({
+        cropResult: imageUrl,
+        image: blob
+      });
+    }, "image/jpeg");
+  };
+
+  onDrop = files => {
+    this.setState({
+      files,
+      fileName: files[0].name
+    });
+  };
+
   render() {
     return (
       <Segment>
@@ -18,14 +77,47 @@ class PhotosPage extends Component {
           <Grid.Row />
           <Grid.Column width={4}>
             <Header color="teal" sub content="Step 1 - Add Photo" />
+            <Dropzone onDrop={this.onDrop} multiple={false}>
+              <div style={{ paddingTop: "30px", textAlign: "center" }}>
+                <Icon name="upload" size="huge" />
+                <Header content="Drop image here or click to add" />
+              </div>
+            </Dropzone>
           </Grid.Column>
           <Grid.Column width={1} />
           <Grid.Column width={4}>
             <Header sub color="teal" content="Step 2 - Resize image" />
+            {this.state.files[0] && (
+              <Cropper
+                style={{ height: 200, width: "100%" }}
+                ref="cropper"
+                src={this.state.files[0].preview}
+                aspectRatio={1}
+                viewMode={0}
+                dragMode="move"
+                guides={false}
+                scalable={true}
+                cropBoxMovable={true}
+                cropBoxResizable={true}
+                crop={this.cropImage}
+              />
+            )}
           </Grid.Column>
           <Grid.Column width={1} />
           <Grid.Column width={4}>
             <Header sub color="teal" content="Step 3 - Preview and Upload" />
+            {this.state.files[0] && (
+              <div>
+                <Image
+                  style={{ minHeight: "200px", minWidth: "200px" }}
+                  src={this.state.cropResult}
+                />
+                <Button.Group>
+                  <Button onClick={this.uploadImage} style={{width: "100px"}} positive icon="check" />
+                  <Button onClick={this.cancelCrop} style={{width: "100px"}} icon="close" />
+                </Button.Group>
+              </div>
+            )}
           </Grid.Column>
         </Grid>
 
@@ -53,4 +145,7 @@ class PhotosPage extends Component {
   }
 }
 
-export default PhotosPage;
+export default connect(
+  null,
+  actions
+)(PhotosPage);
--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,18 +1,97 @@
 import moment from 'moment';
-import { toastr } from 'react-redux-toastr'
+import { toastr } from 'react-redux-toastr';
+import cuid from 'cuid';
+import { asyncActionError, asyncActionStart, asyncActionFinish } from '../async/asyncActions'
 
-export const updateProfile = (user) =>
+export const updateProfile = user => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  const { isLoaded, isEmpty, ...updatedUser } = user;
+  if (updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
+    updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate();
+  }
+
+  try {
+    await firebase.updateProfile(updatedUser);
+    toastr.success('Success', 'Profile updated');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: fileName
+  };
+  try {
+    // upload the file to fb storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get url of image
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+    // get the userdoc from firestore
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // check if user has photo, if not update profile
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL
+      });
+      await user.updateProfile({
+        photoURL: downloadURL
+      });
+    }
+    // add the new photo to photos collection
+    await firestore.add({
+      collection: 'users',
+      doc: user.uid,
+      subcollections: [{collection: 'photos'}]
+    }, {
+      name: fileName,
+      url: downloadURL
+    })
+  } catch (error) {
+    console.log(error);
+    throw new Error('Problem uploading photo')
+  }
+};
+
+export const deletePhoto = (photo) => 
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    try {
+      await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`)
+      await firestore.delete({
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{collection: 'photos', doc: photo.id}]
+      })
+    } catch (error) {
+      console.log(error);
+      throw new Error('Problem deleting the photo')
+    }
+  }
+
+export const setMainPhoto = photo =>
   async (dispatch, getState, {getFirebase}) => {
     const firebase = getFirebase();
-    const {isLoaded, isEmpty, ...updatedUser} = user;
-    if (updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
-      updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate();
-    }
-
     try {
-      await firebase.updateProfile(updatedUser);
-      toastr.success('Success', 'Profile updated')
+      return await firebase.updateProfile({
+        photoURL: photo.url
+      })
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      throw new Error('Problem setting main photo')
     }
   }
