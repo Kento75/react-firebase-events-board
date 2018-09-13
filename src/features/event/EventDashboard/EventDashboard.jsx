76717d0a056a009c30bbdcd1d4b12df5a7d70587
Kento75 <kento2github@gmail.com>
2018-09-13 21:22:30 +0900

    Add InfinitScroll

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
-import { getEventsForDashboard } from '../eventActions';
-import EventList from '../EventList/EventList';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import EventActivity from '../EventActivity/EventActivity';
+import React, { Component } from "react";
+import { Grid, Loader } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { firestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { getEventsForDashboard } from "../eventActions";
+import EventList from "../EventList/EventList";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import EventActivity from "../EventActivity/EventActivity";
 
 const mapState = state => ({
   events: state.events,
@@ -17,32 +17,72 @@ const actions = {
 };
 
 class EventDashboard extends Component {
+  state = {
+    moreEvents: false,
+    loadingInitial: true,
+    loadedEvents: []
+  };
 
-  componentDidMount() {
-    this.props.getEventsForDashboard();
+  async componentDidMount() {
+    let next = await this.props.getEventsForDashboard();
+    console.log(next);
+
+    if (next && next.docs && next.docs.length > 1) {
+      this.setState({
+        moreEvents: true,
+        loadingInitial: false
+      });
+    }
   }
 
-  handleDeleteEvent = eventId => () => {
-    this.props.deleteEvent(eventId);
+  componentWillReceiveProps(nextProps) {
+    if (this.props.events !== nextProps.events) {
+      this.setState({
+        loadedEvents: [...this.state.loadedEvents, ...nextProps.events]
+      });
+    }
+  }
+
+  getNextEvents = async () => {
+    const { events } = this.props;
+    let lastEvent = events && events[events.length - 1];
+    console.log(lastEvent);
+    let next = await this.props.getEventsForDashboard(lastEvent);
+    console.log(next);
+    if (next && next.docs && next.docs.length <= 1) {
+      this.setState({
+        moreEvents: false
+      });
+    }
   };
 
   render() {
-    const { events, loading } = this.props;
-    if (loading) return <LoadingComponent inverted={true} />;
+    const { loading } = this.props;
+    const { moreEvents, loadedEvents } = this.state;
+    if (this.state.loadingInitial) return <LoadingComponent inverted={true} />;
 
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
+          <EventList
+            loading={loading}
+            moreEvents={moreEvents}
+            events={loadedEvents}
+            getNextEvents={this.getNextEvents}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
           <EventActivity />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Loader active={loading} />
         </Grid.Column>
       </Grid>
     );
   }
 }
 
-export default connect(mapState, actions)(
-  firestoreConnect([{ collection: 'events' }])(EventDashboard)
-);
+export default connect(
+  mapState,
+  actions
+)(firestoreConnect([{ collection: "events" }])(EventDashboard));
--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,13 +1,13 @@
-import { toastr } from 'react-redux-toastr';
-import { DELETE_EVENT, FETCH_EVENTS } from './eventConstants';
+import { toastr } from "react-redux-toastr";
+import { DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
-} from '../async/asyncActions';
-import { fetchSampleData } from '../../app/data/mockAPI';
-import { createNewEvent } from '../../app/common/util/helpers';
-import moment from 'moment';
+} from "../async/asyncActions";
+import { fetchSampleData } from "../../app/data/mockAPI";
+import { createNewEvent } from "../../app/common/util/helpers";
+import moment from "moment";
 import firebase from "../../app/config/firebase";
 
 export const createEvent = event => {
@@ -24,9 +24,9 @@ export const createEvent = event => {
         eventDate: event.date,
         host: true
       });
-      toastr.success('Success', 'Event has been created');
+      toastr.success("Success", "Event has been created");
     } catch (error) {
-      toastr.error('Oops', 'Something went wrong');
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
@@ -40,10 +40,10 @@ export const updateEvent = event => {
     }
     try {
       await firestore.update(`events/${event.id}`, event);
-      toastr.success('Success', 'Event has been updated');
+      toastr.success("Success", "Event has been updated");
     } catch (error) {
       console.log(error);
-      toastr.error('Oops', 'Something went wrong');
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
@@ -55,8 +55,8 @@ export const cancelToggle = (cancelled, eventId) => async (
 ) => {
   const firestore = getFirestore();
   const message = cancelled
-    ? 'Are you sure you want to cancel the event?'
-    : 'This reactivate the event - are you sure?';
+    ? "Are you sure you want to cancel the event?"
+    : "This reactivate the event - are you sure?";
   try {
     toastr.confirm(message, {
       onOk: () =>
@@ -69,25 +69,52 @@ export const cancelToggle = (cancelled, eventId) => async (
   }
 };
 
-export const getEventsForDashboard = () => 
-  async (dispatch, getState) => {
-    let today = new Date(Date.now());
-    const firestore = firebase.firestore();
-    const eventsQuery = firestore.collection("events").where("date", ">=", today); 
-    console.log(eventsQuery);
-    try {
-      dispatch(asyncActionStart());
-      let querySnap = await eventsQuery.get();
-      let events = [];
+export const getEventsForDashboard = lastEvent => async (
+  dispatch,
+  getState
+) => {
+  let today = new Date(Date.now());
+  const firestore = firebase.firestore();
+  const eventsRef = firestore.collection("events");
+  try {
+    dispatch(asyncActionStart());
+    let startAfter =
+      lastEvent &&
+      (await firestore
+        .collection("events")
+        .doc(lastEvent.id)
+        .get());
+    let query;
 
-      for (let i = 0; i < querySnap.docs.length; i++) {
-        let evt = {...querySnap.docs[i].data(), id: querySnap.docs[i].id};
-        events.push(evt);
-      }
-      dispatch({type: FETCH_EVENTS, payload: {events}});
+    lastEvent
+      ? (query = eventsRef
+          .where("date", ">=", today)
+          .orderBy("date")
+          .startAfter(startAfter)
+          .limit(2))
+      : (query = eventsRef
+          .where("date", ">=", today)
+          .orderBy("date")
+          .limit(2));
+
+    let querySnap = await query.get();
+
+    if (querySnap.docs.length === 0) {
       dispatch(asyncActionFinish());
-    } catch(error) {
-      console.log(error);
-      dispatch(asyncActionError());
+      return querySnap;
     }
-  } 
+
+    let events = [];
+
+    for (let i = 0; i < querySnap.docs.length; i++) {
+      let evt = { ...querySnap.docs[i].data(), id: querySnap.docs[i].id };
+      events.push(evt);
+    }
+    dispatch({ type: FETCH_EVENTS, payload: { events } });
+    dispatch(asyncActionFinish());
+    return querySnap;
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
+  }
+};
