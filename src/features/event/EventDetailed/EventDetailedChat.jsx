836966ce3682bc5eca5a7e2faeff581e6dd5696a
Kento75 <kento2github@gmail.com>
2018-09-15 21:00:53 +0900

    Add EventDetailedChatForm push comments to realtime database

--- src/features/event/EventDetailed/EventDetailedChat.jsx
+++ src/features/event/EventDetailed/EventDetailedChat.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Segment, Header, Comment, Form, Button } from 'semantic-ui-react';
+import React from "react";
+import { Segment, Header, Comment, Form, Button } from "semantic-ui-react";
+import EventDetailedChatForm from "./EventDetailedChatForm";
 
-const EventDetailedChat = () => {
+const EventDetailedChat = ({addEventComment, eventId}) => {
   return (
     <div>
       <Segment
@@ -9,7 +10,7 @@ const EventDetailedChat = () => {
         attached="top"
         inverted
         color="teal"
-        style={{ border: 'none' }}
+        style={{ border: "none" }}
       >
         <Header>Chat about this event</Header>
       </Segment>
@@ -29,64 +30,8 @@ const EventDetailedChat = () => {
               </Comment.Actions>
             </Comment.Content>
           </Comment>
-
-          <Comment>
-            <Comment.Avatar src="/assets/user.png" />
-            <Comment.Content>
-              <Comment.Author as="a">Elliot Fu</Comment.Author>
-              <Comment.Metadata>
-                <div>Yesterday at 12:30AM</div>
-              </Comment.Metadata>
-              <Comment.Text>
-                <p>
-                  This has been very useful for my research. Thanks as well!
-                </p>
-              </Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-            <Comment.Group>
-              <Comment>
-                <Comment.Avatar src="/assets/user.png" />
-                <Comment.Content>
-                  <Comment.Author as="a">Jenny Hess</Comment.Author>
-                  <Comment.Metadata>
-                    <div>Just now</div>
-                  </Comment.Metadata>
-                  <Comment.Text>Elliot you are always so right :)</Comment.Text>
-                  <Comment.Actions>
-                    <Comment.Action>Reply</Comment.Action>
-                  </Comment.Actions>
-                </Comment.Content>
-              </Comment>
-            </Comment.Group>
-          </Comment>
-
-          <Comment>
-            <Comment.Avatar src="/assets/user.png" />
-            <Comment.Content>
-              <Comment.Author as="a">Joe Henderson</Comment.Author>
-              <Comment.Metadata>
-                <div>5 days ago</div>
-              </Comment.Metadata>
-              <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-
-          <Form reply>
-            <Form.TextArea />
-            <Button
-              content="Add Reply"
-              labelPosition="left"
-              icon="edit"
-              primary
-            />
-          </Form>
         </Comment.Group>
+          <EventDetailedChatForm addEventComment={addEventComment} eventId={eventId} />
       </Segment>
     </div>
   );
--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { withFirestore } from 'react-redux-firebase';
-import EventDetailedHeader from './EventDetailedHeader';
-import EventDetailedInfo from './EventDetailedInfo';
-import EventDetailedChat from './EventDetailedChat';
-import EventDetailedSidebar from './EventDetailedSidebar';
-import { objectToArray } from '../../../app/common/util/helpers';
-import { goingToEvent, cancelGoingToEvent } from '../../user/userActions';
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { withFirestore, firebaseConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import EventDetailedHeader from "./EventDetailedHeader";
+import EventDetailedInfo from "./EventDetailedInfo";
+import EventDetailedChat from "./EventDetailedChat";
+import EventDetailedSidebar from "./EventDetailedSidebar";
+import { objectToArray } from "../../../app/common/util/helpers";
+import { goingToEvent, cancelGoingToEvent } from "../../user/userActions";
+import { addEventComment } from "../eventActions";
 
 const mapState = state => {
   let event = {};
@@ -24,7 +26,8 @@ const mapState = state => {
 
 const actions = {
   goingToEvent,
-  cancelGoingToEvent
+  cancelGoingToEvent,
+  addEventComment
 };
 
 class EventDetailedPage extends Component {
@@ -39,7 +42,7 @@ class EventDetailedPage extends Component {
   }
 
   render() {
-    const { event, auth, goingToEvent, cancelGoingToEvent } = this.props;
+    const { event, auth, goingToEvent, cancelGoingToEvent, addEventComment } = this.props;
     const attendees =
       event && event.attendees && objectToArray(event.attendees);
     const isHost = event.hostUid === auth.uid;
@@ -55,7 +58,7 @@ class EventDetailedPage extends Component {
             cancelGoingToEvent={cancelGoingToEvent}
           />
           <EventDetailedInfo event={event} />
-          <EventDetailedChat />
+          <EventDetailedChat addEventComment={addEventComment} eventId={event.id} />
         </Grid.Column>
         <Grid.Column width={6}>
           <EventDetailedSidebar attendees={attendees} />
@@ -65,4 +68,11 @@ class EventDetailedPage extends Component {
   }
 }
 
-export default withFirestore(connect(mapState, actions)(EventDetailedPage));
+export default compose(
+  withFirestore,
+  connect(
+    mapState,
+    actions
+  ),
+  firebaseConnect((props) => ([`event_chat/${props.match.params.id}`]))
+)(EventDetailedPage);
