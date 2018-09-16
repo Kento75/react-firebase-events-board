5cc81847dea2205822e293cb4a6f6f16b09de246
Kento75 <kento2github@gmail.com>
2018-09-17 06:56:02 +0900

    Add FollowUser insert logic

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -10,7 +10,7 @@ import UserDetailedSidebar from './UserDetailedSidebar'
 import UserDetailedEvents from './UserDetailedEvents'
 import { userDetailedQuery } from '../userQueries'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
-import { getUserEvents } from "../userActions";
+import { getUserEvents, followUser } from "../userActions";
 
 const mapState = (state, ownProps) => {
   let userUid = null;
@@ -34,7 +34,8 @@ const mapState = (state, ownProps) => {
 }
 
 const actions = {
-  getUserEvents
+  getUserEvents,
+  followUser
 }
 
 class UserDetailedPage extends Component {
@@ -49,7 +50,7 @@ class UserDetailedPage extends Component {
   }
 
   render() {
-    const {profile, photos, auth, match, requesting, events, eventsLoading} = this.props;
+    const { profile, photos, auth, match, requesting, events, eventsLoading, followUser } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
     const loading = Object.values(requesting).some(a => a === true);
 
@@ -58,7 +59,7 @@ class UserDetailedPage extends Component {
       <Grid>
         <UserDetailedHeader profile={profile}/>
         <UserDetailedDescription profile={profile}/>
-        <UserDetailedSidebar isCurrentUser={isCurrentUser}/>
+        <UserDetailedSidebar profile={profile} followUser={followUser} isCurrentUser={isCurrentUser}/>
         {photos && photos.length > 0 &&
         <UserDetailedPhotos photos={photos}/>}
         <UserDetailedEvents events={events} eventsLoading={eventsLoading} changeTab={this.changeTab}/>
