b8ce06a02aa231fc0c57ab5f060e14bc44fbbe6c
Kento75 <kento2github@gmail.com>
2018-09-17 19:08:47 +0900

    Fix anonymous user login modal cancel logic

--- src/features/modals/UnauthModal.jsx
+++ src/features/modals/UnauthModal.jsx
@@ -1,41 +1,54 @@
-import React, {Component} from 'react';
-import {Modal, Button, Divider} from 'semantic-ui-react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { Modal, Button, Divider } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import {closeModal, openModal} from "./modalActions";
+import { closeModal, openModal } from "./modalActions";
 
-const actions = {closeModal, openModal};
+const actions = { closeModal, openModal };
 
 class UnauthModal extends Component {
-    render() {
-        const {openModal, closeModal} = this.props;
-        return (
-            <Modal
-                size='mini'
-                open={true}
-                onClose={closeModal}
-            >
-                <Modal.Header>
-                    You need to be signed in to do that!
-                </Modal.Header>
-                <Modal.Content>
-                    <Modal.Description>
-                        <p>Please either login or register to see this page</p>
-                        <Button.Group widths={4}>
-                            <Button fluid color='teal' onClick={() => openModal("LoginModal")}>Login</Button>
-                            <Button.Or />
-                            <Button fluid positive onClick={() => openModal("RegisterModal")}>Register</Button>
-                        </Button.Group>
-                        <Divider/>
-                        <div style={{textAlign: 'center'}}>
-                            <p>Or click cancel to continue as a guest</p>
-                            <Button onClick={closeModal}>Cancel</Button>
-                        </div>
-                    </Modal.Description>
-                </Modal.Content>
-            </Modal>
-        );
-    }
+  handleCloseModal = () => {
+    this.props.history.goBack();
+    this.props.closeModal();
+  };
+
+  render() {
+    const { openModal, closeModal } = this.props;
+    return (
+      <Modal size="mini" open={true} onClose={this.handleCloseModal}>
+        <Modal.Header>You need to be signed in to do that!</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <p>Please either login or register to see this page</p>
+            <Button.Group widths={4}>
+              <Button
+                fluid
+                color="teal"
+                onClick={() => openModal("LoginModal")}
+              >
+                Login
+              </Button>
+              <Button.Or />
+              <Button fluid positive onClick={() => openModal("RegisterModal")}>
+                Register
+              </Button>
+            </Button.Group>
+            <Divider />
+            <div style={{ textAlign: "center" }}>
+              <p>Or click cancel to continue as a guest</p>
+              <Button onClick={this.handleCloseModal}>Cancel</Button>
+            </div>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+    );
+  }
 }
 
-export default connect(null, actions)(UnauthModal);
+export default withRouter(
+  connect(
+    null,
+    actions
+  )(UnauthModal)
+);
